969bdb166d85e0bef4b8a408911f2c1b8bde37dd
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-02-20 21:00:46 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 // To run backend =>npm run server
 //  we are able to use import in backend inplace of require cuz we write "type":"module" in package.json file
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,9 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // used to work with req.body
+app.use(cookieParser()); // use to get data from cookies
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, resp) => {
 //   resp.send("Hello world");

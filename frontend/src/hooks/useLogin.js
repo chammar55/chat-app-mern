de50bcb04623bebeae468f10419d9e065882fa97
Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-11-09 15:02:26 +0500

    .

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -13,14 +13,17 @@ const useLogin = () => {
     if (!success) return;
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-      });
+      const res = await fetch(
+        "https://chat-app-mern-backend-theta.vercel.app/api/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
       const data = await res.json();
       if (data.error) {
         throw new Error(data.error);

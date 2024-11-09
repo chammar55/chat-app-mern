adb909fe4e45ae5816ba0ddac331407cb3fe3858
Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-11-09 15:09:10 +0500

    api path correction

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -9,7 +9,9 @@ const useGetConversations = () => {
     const getConversations = async () => {
       setLoading(true);
       try {
-        const res = await fetch("/api/users");
+        const res = await fetch(
+          "https://chat-app-mern-backend-theta.vercel.app/api/users"
+        );
         const data = await res.json();
         if (data.error) {
           throw new Error(data.error);
